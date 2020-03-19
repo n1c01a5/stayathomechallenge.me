b9d8bb7ac46630914335da676730298e484a9eb9
Nicolas Wagner <contact@wagner-nicolas.com>
2020-03-19 17:25:40 +0100

    first commit

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+import plugin0 from '/Users/n1c0/stayathomechallenge.me/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "/Users/n1c0/stayathomechallenge.me/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/n1c0/stayathomechallenge.me/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/Users/n1c0/stayathomechallenge.me/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/n1c0/stayathomechallenge.me",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/Users/n1c0/stayathomechallenge.me/src/pages/404.js': require('/Users/n1c0/stayathomechallenge.me/src/pages/404.js').default,
+'/Users/n1c0/stayathomechallenge.me/src/pages/about.js': require('/Users/n1c0/stayathomechallenge.me/src/pages/about.js').default,
+'/Users/n1c0/stayathomechallenge.me/src/pages/blog.js': require('/Users/n1c0/stayathomechallenge.me/src/pages/blog.js').default,
+'/Users/n1c0/stayathomechallenge.me/src/pages/index.js': require('/Users/n1c0/stayathomechallenge.me/src/pages/index.js').default,
+'/Users/n1c0/stayathomechallenge.me/src/containers/Post': require('/Users/n1c0/stayathomechallenge.me/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/Users/n1c0/stayathomechallenge.me/src/pages/404.js'
+
